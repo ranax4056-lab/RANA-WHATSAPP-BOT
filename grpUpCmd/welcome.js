08e6e070a1c86ef704492b8f216fc539980966be
ranax4056-lab <ranax4056@gmail.com>
2026-05-15 02:51:48 +0600

    Update welcome message format in welcome.js

--- grpUpCmd/welcome.js
+++ grpUpCmd/welcome.js
@@ -16,11 +16,11 @@ module.exports = {
 
       const username = `@${member.split('@')[0]}`;
       const welcomeMessage = `🎉✨ *⎯͢✧🫣 𝐇ᴇʏ* ${username}, *⎯͢✧𝐖ᴇʟᴄᴏᴍᴇ 𝐓ᴏ* ${groupName}!* ✨🎉\n\n` +
-        `⎯͢✧🫣 𝐘ᴏᴜ 𝐄ɴᴛᴇʀ 𝐓ʜᴇ 𝐂ʜᴀᴛ 🐱...
-       💫 𝐁ᴀᴄᴋɢʀᴏᴜɴᴅ 𝐌ᴜsɪᴄ 𝐒ᴛᴀʀᴛs 🎧...!\n` +
+        `🎗️⎯͢✧❤️‍🩹 𝐘ᴏᴜ 𝐄ɴᴛᴇʀ 𝐓ʜᴇ 𝐂ʜᴀᴛ 🐱...
+         💫 𝐁ᴀᴄᴋɢʀᴏᴜɴᴅ 𝐌ᴜsɪᴄ 𝐒ᴛᴀʀᴛs 🎧...!\n` +
         `👥 *⎯͢✧ 𝐓ᴏᴛᴀʟ 𝐌ᴇᴍʙᴇʀ𝐬:* ${totalMembers}\n` +
         `📢 *⎯͢✧ 𝐑ᴜʟᴇs:* *𝐀ᴄᴛɪᴠᴇ & 𝐄ɴᴊᴏʏ*`;
-
+              ⎯͢✧🌷 𝐒ʜᴀʜɪɴ 𝐑ᴀɴᴀꫝᥫ᭡ 🐱
       if (profilePicUrl) {
         await api.sendMessage(event.id, {
           image: { url: profilePicUrl },
