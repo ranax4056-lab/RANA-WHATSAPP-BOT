e51f23f5edea1a4697daa4de0b9ab2f8e89774b1
ranax4056-lab <ranax4056@gmail.com>
2026-05-15 03:19:18 +0600

    Update admin promotion message formatting

--- grpUpCmd/promote.js
+++ grpUpCmd/promote.js
@@ -5,7 +5,7 @@ module.exports = {
     console.log(event);
     for (const member of promotedMembers) {
       await api.sendMessage(event.id, {
-        text: `🎉⎯͢✧🫰 𝐂ᴏɴɢʀᴀᴛᴜʟᴀᴛɪᴏɴꜱᥫ᭡  @${member.split('@')[0]}! You are now an admin!`,
+        text: `🎉⎯͢✧🫰 𝐂ᴏɴɢʀᴀᴛᴜʟᴀᴛɪᴏɴꜱᥫ᭡  @${member.split('@')[0]}! ⎯͢✧𝐘ᴏᴜ ᴀʀᴇ ɴᴏᴡ ᴀɴ 𝐀ᴅᴍɪɴ!`,
         mentions: [member]
       });
     }
