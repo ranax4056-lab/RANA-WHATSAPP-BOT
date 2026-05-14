debc4c3b055b85bd26e6ad2d94d506e16791387a
ranax4056-lab <ranax4056@gmail.com>
2026-05-15 02:00:02 +0600

    Enhance goodbye message format for removed members

--- grpUpCmd/gdbye.js
+++ grpUpCmd/gdbye.js
@@ -1,12 +1,26 @@
 module.exports = {
   event: 'remove',
+
   handle: async ({ api, event }) => {
+
     const removedMembers = event.participants;
+
     for (const member of removedMembers) {
+
+      const numberTag = member.split('@')[0];
+
       await api.sendMessage(event.id, {
-        text: `Goodbye @${member.split('@')[0]}, we'll miss you!`,
+        text:
+`╔═══❖•ೋ°°ೋ•❖═══╗
+👋 *𝐆ᴏᴏᴅʙʏᴇ @${numberTag}*
+╚═══❖•ೋ°°ೋ•❖═══╝
+
+🤗 *আমাদের ছোট পরিবার তুমাকে মিস করবে..!!! 😅*🫰😌
+
+*⎯͢✧🫣 𝐒ʜᴀʜɪɴ 𝐑ᴀɴᴀꫝᥫ᭡*🐱`,
         mentions: [member]
       });
+
     }
   }
 };
