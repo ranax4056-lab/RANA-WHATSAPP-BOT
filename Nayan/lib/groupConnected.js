a73bcbb0cdaca567a57f81330f6b6899622c76e1
ranax4056-lab <ranax4056@gmail.com>
2026-05-16 20:41:30 +0600

    Modify connection message format
    Update connection message in groupConnected.js

--- Nayan/lib/groupConnected.js
+++ Nayan/lib/groupConnected.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   return `${global.config.botName} CONNECTED«
 
-Assalamualaykum☘️
+⎯͢✧🌷 𝐀ꜱꜱᴀʟᴀᴍᴜᴀʟᴀɪᴋᴜᴍᥫ᭡🍁
 <------------------------------>
 BOT CONNECTED SUCCESSFUL !!!
 
